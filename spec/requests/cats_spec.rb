5cb3a7c54ff2746281b1829415b3b63803251270
Charlean Baxter <baxtercm@outlook.com>
2023-01-30 23:55:13 -0800

    lecture notes for api validations

--- spec/requests/cats_spec.rb
+++ spec/requests/cats_spec.rb
@@ -43,9 +43,30 @@ RSpec.describe "Cats", type: :request do
 
       # payload
       cat = Cat.first
-      p "cat:", cat
+      # p "cat:", cat
       expect(cat.name).to eq('Kevin')
       expect(cat.enjoys).to eq('looking out the window')
+    end
+
+    it 'will not create a cat missing its name' do
+      # create params
+      cat_params = {
+        cat: {
+          age: 9,
+          enjoys: 'looking out the window',
+          image: 'https://images.unsplash.com/photo-1607374035509-704bee7e1aa2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGNhdCUyMGNhcnRvb258ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60'
+        }
+      }
+
+      # make request for create endpoint
+      post '/cats', params: cat_params
+
+      # failing status code - unprocessable entity 422
+      expect(response).to have_http_status(422)
+      # payload
+      cat = JSON.parse(response.body)
+      p "cat:", cat
+      expect(cat['name']).to include("can't be blank")
     end
   end
 end
